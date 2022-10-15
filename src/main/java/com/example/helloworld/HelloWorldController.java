351ff922b0720016fe43625fb36382ba8b9b6b95
yuika nagata <xxx.yu.74@gmail.com>
2022-10-15 15:56:13 +0900

    クエリ文字列の取得

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -17,50 +18,50 @@ public class HelloWorldController {
     }
 
 
-    LocalDateTime now = LocalDateTime.now();
-    DateTimeFormatter Time = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-    String nowTime = now.format(Time);
+    LocalTime nowLocalTime = LocalTime.now();
+    LocalTime morning = LocalTime.of(8, 59);
+    LocalTime noon = LocalTime.of(17, 59);
+    LocalTime night = LocalTime.of(23, 59);
+    LocalTime midnight = LocalTime.of(04, 00);
 
 
-    public static final LocalTime nowLocalTime = LocalTime.now();
-    public static final LocalTime morning = LocalTime.of(8, 59);
-    public static final LocalTime noon = LocalTime.of(17, 59);
-    public static final LocalTime night = LocalTime.of(23, 59);
-    public static final LocalTime midnight = LocalTime.of(04, 00);
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam String country) {
 
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String nowTime = now.format(timeFormat);
 
-    @GetMapping("/japan")
-    public String greeting() {
+        LocalTime nowLocalTime = LocalTime.now();
+
         //04:00から08:59までは「おはようございます」を表示
-        if (nowLocalTime.isAfter(midnight) && nowLocalTime.isBefore(morning) || nowLocalTime.equals(morning) || nowLocalTime.equals(midnight)) {
+        if (country.equals("japan") && nowLocalTime.isAfter(midnight) && nowLocalTime.isBefore(morning) || nowLocalTime.equals(morning) || nowLocalTime.equals(midnight)) {
             return nowTime + "   　　ゲストさん　　" + "おはようございます";
         }
         //09:00から17:59までは「こんにちわ」を表示
-        else if (nowLocalTime.isAfter(morning) && nowLocalTime.isBefore(noon) || nowLocalTime.equals(noon)) {
+        else if (country.equals("japan") && nowLocalTime.isAfter(morning) && nowLocalTime.isBefore(noon) || nowLocalTime.equals(noon)) {
             return nowTime + "   　　ゲストさん　　" + "こんにちわ";
         }
         //18:00から03:59までは「こんばんわ」を表示
-        else if (nowLocalTime.isAfter(noon) && nowLocalTime.isBefore(night) || nowLocalTime.isBefore(midnight) || nowLocalTime.equals(night)) {
+        else if (country.equals("japan") && nowLocalTime.isAfter(noon) && nowLocalTime.isBefore(night) || nowLocalTime.isBefore(midnight) || nowLocalTime.equals(night)) {
             return nowTime + "   　　ゲストさん　　" + "こんばんわ";
         }
-        return null;
-    }
-
-    @GetMapping("/usa")
-    public String usaGreeting() {
-        //04:00から08:59までは「おはようございます」を表示
-        if (nowLocalTime.isAfter(midnight) && nowLocalTime.isBefore(morning) || nowLocalTime.equals(morning) || nowLocalTime.equals(midnight)) {
+        //04:00から08:59までは「Good morning」を表示
+        else if (country.equals("usa") && nowLocalTime.isAfter(midnight) && nowLocalTime.isBefore(morning) || nowLocalTime.equals(morning) || nowLocalTime.equals(midnight)) {
             return nowTime + "   　　ゲストさん　　" + "Good morning";
         }
-        //09:00から17:59までは「こんにちわ」を表示
-        else if (nowLocalTime.isAfter(morning) && nowLocalTime.isBefore(noon) || nowLocalTime.equals(noon)) {
+        //09:00から17:59までは「Good afternoon」を表示
+        else if (country.equals("usa") && nowLocalTime.isAfter(morning) && nowLocalTime.isBefore(noon) || nowLocalTime.equals(noon)) {
             return nowTime + "   　　ゲストさん　　" + "Good afternoon";
         }
-        //18:00から03:59までは「こんばんわ」を表示
-        else if (nowLocalTime.isAfter(noon) && nowLocalTime.isBefore(night) || nowLocalTime.isBefore(midnight) || nowLocalTime.equals(night)) {
+        //18:00から03:59までは「Good evening」を表示
+        else if (country.equals("usa") && nowLocalTime.isAfter(noon) && nowLocalTime.isBefore(night) || nowLocalTime.isBefore(midnight) || nowLocalTime.equals(night)) {
             return nowTime + "   　　ゲストさん　　" + "Good evening";
         }
         return null;
     }
+
+
 }
+
 
